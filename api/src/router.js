059e83de71f45ce59924b35c56c3a1c78b9c623e
Matthew Gonzales <gonzalesmattg@gmail.com>
2022-02-17 03:04:12 -0600

    followed prompt to create parent thread

--- api/src/router.js
+++ api/src/router.js
@@ -2,10 +2,11 @@ const { Router } = require('express');
 const { sendFile } = require('express/lib/response');
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor');
 
 const router = Router();
 
-const photoPath = path.resolve(__dirname,'../../client/photo-viewer.html');
+const photoPath = path.resolve(__dirname, '../../client/photo-viewer.html');
 
 const storage = multer.diskStorage({
   destination: 'api/uploads/',
@@ -30,12 +31,17 @@ const upload = multer({
   storage,
 });
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
   if (request.fileValidationError) {
     return response.status(400).json({
       error: request.fileValidationError,
     });
   } else {
+    try {
+      await imageProcessor(request.file.filename);
+    } catch (error) {
+      
+    }
     return response.status(201).json({
       success: true,
     });
@@ -44,6 +50,6 @@ router.post('/upload', upload.single('photo'), (request, response) => {
 
 router.get('/photo-viewer', (request, response) => {
   response.sendFile(photoPath);
-})
+});
 
 module.exports = router;
